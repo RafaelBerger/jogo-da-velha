b7975eb2df391ce152ec757622c8e99badf2fff5
Rafael Berger <rafaelberger.dev@gmail.com>
2022-06-10 02:05:44 -0300

    recomeçando a logica

--- script.js
+++ script.js
@@ -1,86 +1,46 @@
-// #region Variáveis
-let vezDoJogador = undefined;
-
-let areaDosBotoes = document.getElementById("escolha-jogador");
-let areaDoJogo = document.getElementById("area-jogo");
-let vencedor = document.getElementById("vencedor");
-let telaDeVitoria = document.getElementById("tela-vitoria");
+// #region handle the screen
 
 const btnO = document.querySelector("#o").addEventListener("click", () => {
-  vezDoJogador = "o";
-  jogoEstadoVisual();
+  playerTurn = "o";
+  board.style.display = "grid";
+  choosePlayer.style.display = "none";
 });
-
 const btnX = document.querySelector("#x").addEventListener("click", () => {
-  vezDoJogador = "x";
-  jogoEstadoVisual();
+  playerTurn = "x";
+  board.style.display = "grid";
+  choosePlayer.style.display = "none";
 });
 
-let movimentosDoO = [];
-let movimentosDoX = [];
+const board = document.querySelector("#area-jogo");
+const choosePlayer = document.querySelector("#escolha-jogador");
+
+//#endregion
+// #region Variáveis
+const quadrados = document.querySelectorAll(".square");
+let combinacoesVitoria = [
+  ["1", "2", "3"],
+  ["4", "5", "6"],
+  ["7", "8", "9"],
+  ["1", "4", "7"],
+  ["2", "5", "8"],
+  ["3", "6", "9"],
+  ["3", "5", "7"],
+  ["1", "5", "9"],
+];
+let playerTurn;
+
 //#endregion
 
-const jogoEstadoVisual = () => {
-  if (vezDoJogador === undefined) {
-    areaDoJogo.style.display = "none";
-  } else {
-    areaDoJogo.style.display = "grid";
-    areaDosBotoes.style.display = "none";
-  }
-};
-
-// jogoEstadoVisual();
-
-document.addEventListener("click", (event) => {
-  if (event.target.id == "body") {
-    return "";
-  } else {
-    if (vezDoJogador == "o" && event.target.innerText == "") {
-      event.target.innerText = "O";
-      movimentosDoO.push(event.target.id);
-      vezDoJogador = "x";
-      // checaVitoria();
-      console.log(movimentosDoO);
-    } else if (vezDoJogador == "x" && event.target.innerText == "") {
-      event.target.innerText = "X";
-      movimentosDoX.push(event.target.id);
-      vezDoJogador = "o";
-      // checaVitoria();
-    }
-    checaVitoria();
-  }
+quadrados.forEach((square) => {
+  square.addEventListener("click", handleClick, { once: true });
 });
 
-function checaVitoria() {
-  if (
-    movimentosDoO.includes(
-      ("1" && "2" && "3") ||
-        ("4" && "5" && "6") ||
-        ("7" && "8" && "9") ||
-        ("1" && "4" && "7") ||
-        ("2" && "5" && "8") ||
-        ("3" && "6" && "9") ||
-        ("3" && "5" && "7") ||
-        ("1" && "5" && "9")
-    ) === true
-  ) {
-    telaDeVitoria.display = "flex";
-    vencedor.innerText = "O jogador O ganhou!";
-  }
-
-  if (
-    movimentosDoX.includes(
-      ("1" && "2" && "3") ||
-        ("4" && "5" && "6") ||
-        ("7" && "8" && "9") ||
-        ("1" && "4" && "7") ||
-        ("2" && "5" && "8") ||
-        ("3" && "6" && "9") ||
-        ("3" && "5" && "7") ||
-        ("1" && "5" && "9")
-    ) === true
-  ) {
-    telaDeVitoria.display = "flex";
-    vencedor.innerText = "O jogador X ganhou!";
+function handleClick(e) {
+  if (playerTurn == "o") {
+    e.target.innerText = "O";
+    playerTurn = "x";
+  } else if (playerTurn == "x") {
+    e.target.innerText = "X";
+    playerTurn = "o";
   }
 }
